666f71548c2d55311b84539bfa8dfee726a2d081
AmrAzzam2004 <amrazzam2444@gmail.com>
2026-04-24 15:42:32 +0300

    recognise the plate image by OCR

--- Car_plate_recognation.py
+++ Car_plate_recognation.py
@@ -3,16 +3,56 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
-import imaplib
+import imutils
 import easyocr  
 import os 
-# IMAGE_DIR = "./images"
+IMAGE_DIR = "images"
+# reader = easyocr.Reader(['en'])
+
+image_paths = [
+    os.path.join(IMAGE_DIR, f)
+    for f in os.listdir(IMAGE_DIR)
+    if f.lower().endswith(('.png', '.jpg', '.jpeg'))
+]
+
+print("image found",image_paths)
+# change the color of the image to gray scale
+img=cv2.imread(image_paths[0])
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# plt.imshow(gray,cmap='gray')
+# plt.show()
+
+#remove noise from the image and detect edges in the image
+bfilter=cv2.bilateralFilter(gray,11,17,17) #noise reduction
+edged=cv2.Canny(bfilter,200,400) #edge detection
+# plt.imshow(edged,cmap='gray')
+# plt.show()
+
+#find contours based on edges detected and keep the largest 10 contours
+keypoints=cv2.findContours(edged.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours=imutils.grab_contours(keypoints)
+contours=sorted(contours,key=cv2.contourArea,reverse=True)[:10]
+
+location=None
+for contour in contours:
+    approx=cv2.approxPolyDP(contour,10,True)
+    if len(approx)==4:
+        location=approx
+        break
+    
+mask=np.zeros(gray.shape,np.uint8)
+new_image=cv2.drawContours(mask,[location],0,255,-1)
+new_image=cv2.bitwise_and(img,img,mask=mask)
+# plt.imshow(new_image)
+# plt.show()
+
+x,y=np.where(mask==255)
+(x1,y1)=np.min(x),np.min(y)
+(x2,y2)=np.max(x),np.max(y)
+cropped_image=gray[x1:x2+1,y1:y2+1]
+# plt.imshow(cropped_image,cmap='gray')
+# plt.show()
+
 reader = easyocr.Reader(['en'])
-
-# image_paths = [
-#     os.path.join(IMAGE_DIR, f)
-#     for f in os.listdir(IMAGE_DIR)
-#     if f.lower().endswith(('.png', '.jpg', '.jpeg'))
-# ]
-
-# print("image found",image_paths)
+result = reader.readtext(cropped_image)
+print(result)
